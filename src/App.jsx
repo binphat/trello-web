8ac67aa1f4cdbf9c6f2ef43d59ff110b8372ebf5
binphat <nguyenphat2203@gmail.com>
2025-03-11 14:43:23 +0700

    Update Dark/Light Mode

--- src/App.jsx
+++ src/App.jsx
@@ -34,17 +34,17 @@ function ModeSelect() {
         onChange={handleChange}
       >
         <MenuItem value="light">
-          <div style={{ display:'flex', alignItems: 'center', gap: '8px'}}>
+          <div style={{ display:'flex', alignItems: 'center', gap: '8px' }}>
             <LightModeIcon fontSize='small'/> Light
           </div>
         </MenuItem>
         <MenuItem value="dark">
-          <Box sx={{ display:'flex', alignItems: 'center', gap: 1}}>
+          <Box sx={{ display:'flex', alignItems: 'center', gap: 1 }}>
             <DarkModeOutlinedIcon fontSize='small'/> Dark
           </Box>
         </MenuItem>
         <MenuItem value="system">
-          <Box sx={{ display:'flex', alignItems: 'center', gap: 1}}>  
+          <Box sx={{ display:'flex', alignItems: 'center', gap: 1 }}>
             <SettingsBrightnessIcon fontSize='small'/> System
           </Box>
         </MenuItem>
