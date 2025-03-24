e02fbb297fdb729376744771c1de84020a358357
binphat <nguyenphat2203@gmail.com>
2025-03-24 23:31:46 +0700

    Board Content

--- src/components/AppBar/index.jsx
+++ src/components/AppBar/index.jsx
@@ -36,7 +36,10 @@ function AppBar() {
       overflowX: 'auto',
       bgcolor: (theme) => (
         theme.palette.mode === 'dark' ? '#2c3e50' : '#1565c0'
-      )
+      ),
+      '&::-webkit-scrollbar-track': {
+        m: 2
+      }
     }}>
       <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }}>
         <AppsIcon sx={{ color: 'white' }} />
