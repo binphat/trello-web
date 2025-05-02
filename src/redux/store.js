2c9c01f49c5c13de8437150fb0b9cef1ce78c32a
binphat <nguyenphat2203@gmail.com>
2025-05-03 00:44:49 +0700

    Lesson 07

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { activeBoardReducer } from './activeBoard/activeBoardSlice'
+import { userReducer } from '~/redux/User/userSlice'
 
 export const store = configureStore({
   reducer: {
-    activeBoard: activeBoardReducer
+    activeBoard: activeBoardReducer,
+    user: userReducer
   }
 })
