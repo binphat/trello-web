8918fb38a02663432958cb434946b91b17249573
binphat <nguyenphat2203@gmail.com>
2025-05-03 02:36:09 +0700

    lesson 8

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,34 @@
 //
-import { Routes, Route, Navigate } from 'react-router-dom'
+import { Routes, Route, Navigate, Outlet } from 'react-router-dom'
 //
 import Board from '~/pages/Boards/_id'
 import NotFound from './pages/404/NotFound(test)'
 import Auth from '~/pages/Auth/Auth.jsx'
 import AccountVerification from './pages/Auth/AccountVerification'
+import { useSelector } from 'react-redux'
+import { selectCurrentUser } from '~/redux/User/userSlice'
 
-
+/**
+ * Giải pháp Clean Code trong việc xác định các
+ * route nào cần đăng nhập tài khoản xong thì mới
+ * cho truy cập
+ *
+ * Sử dụng <Outlet /> của react-router-dom để hiển
+ * thị các Child Route (xem cách sử dụng trong App()
+ * bên dưới)
+ * https://reactrouter.com/en/main/components/outlet
+ *
+ * Một bài hướng dẫn khá đầy đủ:
+ * https://www.robinwieruch.de/react-router-
+ * private-routes/
+ */
+const ProtectedRoute = ({ user }) => {
+  console.log('🚀 ~ ProtectedRoute ~ user:', user)
+  if (!user) return <Navigate to='/login' replace={true}/>
+  return <Outlet />
+}
 function App() {
+  const currentUser = useSelector(selectCurrentUser)
   return (
     <Routes>
       <Route path='/' element={
@@ -16,10 +37,17 @@ function App() {
         // bằng nút back của trình duyệt giữa 2 trường hợp có replace hoặc không có.
         <Navigate to="/boards/6800a4e43d2e90f5dd5bd70c" replace={true} />
       } />
-      {/* React Router Dom/boards/{board_id} */}
-      {/* Board Details */}
-      <Route path='/boards/:boardId' element={<Board />} />
+      {/* Protected Routes (Hiểu đơn giản trong dự án
+        của chúng ta là những route chỉ cho truy cập sau
+        khi đã login) */}
+      <Route element={<ProtectedRoute user={currentUser} />}>
 
+        {/* React Router Dom/boards/{board_id} */}
+        {/* <Outlet /> của react-router-dom sẽ chạy vào child route trong này */}
+        {/* Board Details */}
+        <Route path='/boards/:boardId' element={<Board />} />
+
+      </Route>
       {/* Authetication*/}
       <Route path='/login' element={<Auth />}/>
       <Route path='/register' element={<Auth />}/>
--- src/main.jsx
+++ src/main.jsx
@@ -18,22 +18,30 @@ import { Provider } from 'react-redux'
 // Cấu hình react-router-dom với BrowserRouter
 import { BrowserRouter } from 'react-router-dom'
 
+// Cấu hình Redux-Persist
+import { PersistGate } from 'redux-persist/integration/react'
+import { persistStore } from 'redux-persist'
+
+const persistor = persistStore(store)
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter basename='/'>
     <Provider store={store}>
-      <CssVarsProvider theme={theme}>
-        <ConfirmProvider defaultOptions={{
-          allowClose: false,
-          dialogProps: { maxWidth: 'xs' },
-          buttonOrder: ['confirm', 'cancel'],
-          cancellationButtonProps: { color: 'inherit' },
-          confirmationButtonProps: { color: 'secondary', variant: 'outlined' }
-        }}>
-          <CssBaseline />
-          <App />
-          <ToastContainer position='top-center' theme='colored' />
-        </ConfirmProvider>
-      </CssVarsProvider>
+      <PersistGate persistor={persistor}>
+        <CssVarsProvider theme={theme}>
+          <ConfirmProvider defaultOptions={{
+            allowClose: false,
+            dialogProps: { maxWidth: 'xs' },
+            buttonOrder: ['confirm', 'cancel'],
+            cancellationButtonProps: { color: 'inherit' },
+            confirmationButtonProps: { color: 'secondary', variant: 'outlined' }
+          }}>
+            <CssBaseline />
+            <App />
+            <ToastContainer position='top-center' theme='colored' />
+          </ConfirmProvider>
+        </CssVarsProvider>
+      </PersistGate>
     </Provider>
   </BrowserRouter>
 )
