316fc83f60dd8591d9b0bdf802e8b405c7cf460d
binphat <nguyenphat2203@gmail.com>
2025-06-09 14:26:59 +0700

    Fix Cors

--- src/redux/User/userSlice.js
+++ src/redux/User/userSlice.js
@@ -45,23 +45,6 @@ export const userSlice = createSlice({
   // extraReducers: Nơi xử lý dữ liệu bất đồng bộ
   extraReducers: (builder) => {
     builder.addCase(loginUserAPI.fulfilled, (state, action) => {
-      // action.payload ở đây chính là cái response.data trả về ở trên
-      const user = action.payload
-      // Update lại dữ liệu của cái currentActiveBoard
-      state.currentUser = user
-    })
-    /**
-     * API logout sau khi gọi thành công thì sẽ clear thông tin currentUser về null ở đây
-     * Kết hợp ProtectedRoute đã làm ở App.js => code sẽ điều hướng chuẩn về trang Login
-     */
-    builder.addCase(logoutUserAPI.fulfilled, (state) => {
-      state.currentUser = null
-    })
-    builder.addCase(updateUserAPI.fulfilled, (state, action) => {
-      const user = action.payload
-      state.currentUser = user
-    })
-    builder.addCase(loginUserAPI.fulfilled, (state, action) => {
       const { user, token } = action.payload
       state.currentUser = user
       state.token = token
@@ -76,6 +59,11 @@ export const userSlice = createSlice({
         localStorage.removeItem('token')
       }
     })
+    builder.addCase(updateUserAPI.fulfilled, (state, action) => {
+      const user = action.payload
+      state.currentUser = user
+    })
+
   }
 })
 // Actions: Là nơi định nghĩa cho các components bên dưới gọi bằng dispatch() tới nó để cập nhật lại dữ liệu thông qua reducer (chạy đồng bộ)
