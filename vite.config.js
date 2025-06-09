94e36048e1dc269bcbc77196156e91aa48e23b76
binphat <nguyenphat2203@gmail.com>
2025-06-09 11:16:54 +0700

    Fix Vercel

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import svgr from 'vite-plugin-svgr'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   define: {
     'process.env': process.env
@@ -11,8 +10,12 @@ export default defineConfig({
     react(),
     svgr()
   ],
-
-  // base: './'
+  base: process.env.NODE_ENV === 'production' ? '/' : '/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false
+  },
   resolve: {
     alias: [
       { find: '~', replacement: '/src' }
